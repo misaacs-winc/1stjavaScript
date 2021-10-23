306e7b1d15ad7032e9c880737198e73a8ac3cd2c
misaacs-winc <88142732+misaacs-winc@users.noreply.github.com>
2021-10-23 04:47:04 +0200

    Add files via upload

---
+++ javaScript/index.js
@@ -0,0 +1,28 @@
+let myName = 'Micah';
+console.log(myName);
+
+sum = 4 + 4
+console.log(sum);
+
+addString = '4' + '4'
+console.log(addString);
+
+multiply = 8 * 7
+console.log(multiply);
+
+substract = 456 - 654
+console.log(substract);
+
+divide = 8 / 2
+console.log(divide);
+
+modulus = 13 % 4
+console.log(modulus);
+
+let age = 84;
+console.log(age);
+console.log(typeof age);
+
+let myAge = 'I am 84 yrs old';
+console.log(myAge);
+console.log(typeof myAge);
